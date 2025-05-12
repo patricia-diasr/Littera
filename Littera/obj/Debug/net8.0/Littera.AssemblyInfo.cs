0b6e84e4d8488e4f79cc07a1e02e7694d56d921e
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2025-05-12 19:46:55 -0300

    Adicionado página de cadastro

--- Littera/obj/Debug/net8.0/Littera.AssemblyInfo.cs
+++ Littera/obj/Debug/net8.0/Littera.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Littera")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c8413a00dc0563f5219d9aa531de19af070b14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3eecfdbea526d161e0e62dc07bcb9f86ed4722")]
 [assembly: System.Reflection.AssemblyProductAttribute("Littera")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Littera")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
