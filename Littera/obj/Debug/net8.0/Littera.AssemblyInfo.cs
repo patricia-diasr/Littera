bfc0ea62b36ccbab4c411715d6ea8d1d8578b499
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2025-06-03 09:34:29 -0300

    Ajuste

--- Littera/obj/Debug/net8.0/Littera.AssemblyInfo.cs
+++ Littera/obj/Debug/net8.0/Littera.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Littera")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a2933116a0af855103a0d2906b7d9ea5e3c9dfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+385de12ebe501c08569c8a3741359c680fdd3fc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Littera")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Littera")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
